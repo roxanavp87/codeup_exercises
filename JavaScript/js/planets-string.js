82f32b0b9608801e56b5094f1a6b19c56289656a
roxanavp87 <roxanavp87@gmail.com>
2017-04-13 11:44:46 -0500

    - extra excercises

--- JavaScript/js/planets-string.js
+++ JavaScript/js/planets-string.js
@@ -25,3 +25,52 @@
     console.log(list);
 
 })();
+
+
+console.log("---------------------------------------------------------------------------");
+console.log("Extra Exercise");
+console.log("---------------------------------------------------------------------------");
+
+function convert24hourTo12hour(string24) {
+    var hour = parseInt(string24.substring(0,2));
+
+    if(hour > 12) {
+        string12 = hour - 12;
+        string12 += string24.substring(2,string24.length) + 'pm';
+    } else if(hour === 0) {
+        string12 = '12' + string24.substring(2,string24.length) + 'am';
+    } else {
+        string12 = hour + string24.substring(2,string24.length) + 'am';
+    }
+    return string12;
+}
+
+console.log(convert24hourTo12hour('10:30'));
+
+
+function convert12hourTo24hour(string12) {
+    var hour = parseInt(string12.split(':')[0]);
+    var min = string12.split(':')[1];
+    min = min.substring(0,min.length-2);
+    var last2 = string12.substring(string12.length-2,string12.length);
+
+    if(last2 === 'am'){
+        if(hour < 10) {
+            string24 = '0' + hour;
+        } else if(hour === 12) {
+            string24 = '00';
+        } else {
+            string24 = hour;
+        }
+    } else {
+        if(hour < 12) {
+            string24 = hour + 12;
+        } else {
+            string24 = hour;
+        }
+    }
+    string24 += ':' + min;
+    return string24;
+}
+
+console.log(convert12hourTo24hour('6:30am'));
