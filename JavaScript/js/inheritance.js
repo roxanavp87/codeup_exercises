40c672556e2551962316aaca6b529c7c12b5be8f
roxanavp87 <roxanavp87@gmail.com>
2017-09-17 21:59:19 -0500

    Classical inheritance with Object.create()

---
+++ JavaScript/js/inheritance.js
@@ -0,0 +1,35 @@
+/**
+ * Created by roxana on 9/17/17.
+ */
+
+// Classical inheritance with Object.create()
+
+// Shape - superclass
+function Shape() {
+    this.x = 0;
+    this.y = 0;
+}
+
+// superclass method
+Shape.prototype.move = function(x, y) {
+    this.x += x;
+    this.y += y;
+    console.info('Shape moved.');
+};
+
+// Rectangle - subclass
+function Rectangle() {
+    Shape.call(this); // call super constructor.
+}
+
+// subclass extends superclass
+Rectangle.prototype = Object.create(Shape.prototype);
+Rectangle.prototype.constructor = Rectangle;
+
+var rect = new Rectangle();
+
+console.log('Is rect an instance of Rectangle?',
+    rect instanceof Rectangle); // true
+console.log('Is rect an instance of Shape?',
+    rect instanceof Shape); // true
+rect.move(1, 1); // Outputs, 'Shape moved.'
